6ab2e49d74a83422714e8c8ea54d4cb3a63e733e
Tomasz Stanisz <fremen1990@gmail.com>
2023-04-13 14:24:00 +0200

    :test_tube: Add test for pinecone.service

--- apps/api/src/messages/pinecone/pinecone.service.spec.ts
+++ apps/api/src/messages/pinecone/pinecone.service.spec.ts
@@ -1,18 +1,59 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { PineconeService } from './pinecone.service';
+import { PineconeClient } from 'pinecone-client';
+
+interface PineconeVectors {
+  [key: string]: number[];
+}
 
 describe('PineconeService', () => {
-  let service: PineconeService;
+  let pineconeService: PineconeService;
+  let pineconeClient: PineconeClient<PineconeVectors>;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [PineconeService],
-    }).compile();
-
-    service = module.get<PineconeService>(PineconeService);
+  beforeAll(() => {
+    process.env.PINECONE_API_KEY = 'mock_api_key';
+    process.env.PINECONE_BASE_URL = 'http://localhost:8080';
+    process.env.PINECONE_NAMESPACE = 'mock_namespace';
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  beforeEach(() => {
+    pineconeClient = {
+      upsert: jest.fn(),
+      query: jest.fn(),
+    } as unknown as PineconeClient<PineconeVectors>;
+    pineconeService = new PineconeService();
+    pineconeService['pinecone'] = pineconeClient;
+  });
+
+  afterAll(() => {
+    jest.resetAllMocks();
+    delete process.env.PINECONE_API_KEY;
+    delete process.env.PINECONE_BASE_URL;
+    delete process.env.PINECONE_NAMESPACE;
+  });
+
+  describe('upsert', () => {
+    it('should call pinecone.upsert with vectors', () => {
+      const vectors = [1, 2, 3];
+      const pineconeVectors: PineconeVectors = { '0': vectors };
+      pineconeService.upsert(vectors);
+      expect(pineconeClient.upsert).toHaveBeenCalledWith({ vectors: [vectors] });
+    });
+  });
+
+  describe('query', () => {
+    it('should call pinecone.query with vector and options', async () => {
+      const vector = [1, 2, 3];
+      const matches = [{ id: '1', score: 0.9 }];
+      (pineconeClient.query as jest.Mock).mockResolvedValue({ matches });
+      const expectedMatches = [1];
+      const actualMatches = await pineconeService.query(vector);
+      expect(pineconeClient.query).toHaveBeenCalledWith({
+        vector: vector,
+        topK: 10,
+        includeMetadata: true,
+        includeValues: false,
+      });
+      expect(actualMatches).toEqual(expectedMatches);
+    });
   });
 });
