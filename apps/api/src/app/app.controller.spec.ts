915f2198f2ee860af241fba04927cc06f6c3f36d
Tomasz Stanisz <fremen1990@gmail.com>
2023-04-13 14:44:13 +0200

    :white_check_mark: configure husky in package.json

--- apps/api/src/app/app.controller.spec.ts
+++ apps/api/src/app/app.controller.spec.ts
@@ -3,7 +3,12 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-describe('AppController', () => {
+
+
+
+describe
+
+('AppController', () => {
   let app: TestingModule;
 
   beforeAll(async () => {
