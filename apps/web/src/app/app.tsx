140265b337011590b281c0cb32bb53dbd1a3984d
Tomasz Stanisz <fremen1990@gmail.com>
2023-04-14 18:40:45 +0200

    :boom: First Gamora AI prototype connection WEB to API

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,11 +1,87 @@
 import gamoraAiDark from '../assets/images/gamora-dark.png';
 
-import { Avatar, Button, ConfigProvider, Layout, Typography } from 'antd';
+import {
+  Avatar,
+  Button,
+  ConfigProvider,
+  Layout,
+  Progress,
+  Typography,
+} from 'antd';
 import { Input, Space, Alert } from 'antd';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const { TextArea } = Input;
 
+const ProgressCircle = ({ duration }: { duration: number }) => {
+  const [percent, setPercent] = useState(0);
+
+  useEffect(() => {
+    const intervalDuration = duration * 10;
+    const interval = setInterval(() => {
+      setPercent((prevPercent) => prevPercent + 1);
+    }, intervalDuration);
+
+    return () => clearInterval(interval);
+  }, [duration]);
+
+  return (
+    <div
+      style={{
+        position: 'fixed',
+        zIndex: 999,
+        height: '100vh',
+        width: '100vw',
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      {/*<Spin style={{ width: '300px', height: '300px' }} />*/}
+      <Progress
+        type="circle"
+        percent={percent}
+        strokeColor={{ '0%': '#108ee9', '100%': '#87d068' }}
+        showInfo={true}
+      />
+    </div>
+  );
+};
+
 export function App() {
+  const [apiStatus, setApiStatus] = useState({});
+  const [questionInput, setQuestionInput] = useState('');
+  const [answer, setAnswer] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const apiHealthCheckApi = async () =>
+    await axios.get('http://localhost:3000/api');
+
+  const questionApi = async (question: string) =>
+    await axios.post('http://localhost:3000/api/messages', {
+      message: question,
+    });
+
+  const handleAskQuestion = async () => {
+    setIsLoading(true);
+    const answer = await questionApi(questionInput);
+    setAnswer(answer.data);
+    setIsLoading(false);
+    console.log('anser', answer);
+  };
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await apiHealthCheckApi();
+        setApiStatus(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+
   return (
     <ConfigProvider
       theme={{
@@ -24,6 +100,7 @@ export function App() {
           justifyContent: 'center',
         }}
       >
+        {isLoading && <ProgressCircle duration={2} />}
         <Typography
           style={{
             fontSize: '34px',
@@ -35,30 +112,43 @@ export function App() {
         >
           Gamora AI
         </Typography>
+        <Typography
+          style={{
+            fontSize: '14px',
+            fontWeight: 'bold',
+            color: '#274916',
+            padding: '5px',
+            margin: '5px',
+          }}
+        >
+          API status: {JSON.stringify(apiStatus)}
+        </Typography>
         <Avatar
           icon={<img src={gamoraAiDark} alt="" />}
           style={{ width: 250, height: 250 }}
         />
-
         <TextArea
-          rows={4}
+          rows={5}
           placeholder="Ask me something..."
-          maxLength={6}
+          maxLength={20000}
           size="middle"
           style={{
             width: '80%',
-            backgroundColor: '#7f7f7f',
+            backgroundColor: '#919090',
             padding: '10px',
             margin: '20px',
           }}
+          onChange={(e) => setQuestionInput(e.target.value)}
         />
-
-        <Button type="primary" size="middle">
+        <Button
+          type="primary"
+          size="middle"
+          onClick={handleAskQuestion}
+          disabled={isLoading}
+        >
           Button
         </Button>
-
         <Space size="large" />
-
         <Alert
           style={{
             backgroundColor: '#d5f2bb',
@@ -66,7 +156,7 @@ export function App() {
             padding: '10px',
             margin: '20px',
           }}
-          description="Info Description Info Description Info Description Info DescriptionInfo Description Info Description Info Description Info DescriptionInfo Description Info Description Info Description Info DescriptionInfo Description Info Description Info Description Info DescriptionInfo Description Info Description Info Description Info DescriptionInfo Description Info Description Info Description Info DescriptionInfo Description Info Description Info Description Info DescriptionInfo Description Info Description Info Description Info DescriptionInfo Description Info Description Info Description Info DescriptionInfo Description Info Description Info Description Info DescriptionInfo Description Info Description Info Description Info DescriptionInfo Description Info Description Info Description Info DescriptionInfo Description Info Description Info Description Info DescriptionInfo Description Info Description Info Description Info DescriptionInfo Description Info Description Info Description Info DescriptionInfo Description Info Description Info Description Info DescriptionInfo Description Info Description Info Description Info Description"
+          description={answer ? answer : 'No answer yet'}
           type="success"
         />
       </Layout>
