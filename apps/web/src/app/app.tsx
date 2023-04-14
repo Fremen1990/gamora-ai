b950be526b56127a33436c2ceecc2e9b5460765f
Tomasz Stanisz <fremen1990@gmail.com>
2023-04-14 02:59:37 +0200

    :construction: First Layout work in progress v2

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,51 +1,76 @@
-import NxWelcome from './nx-welcome';
+import gamoraAiDark from '../assets/images/gamora-dark.png';
 
-import { Route, Routes, Link } from 'react-router-dom';
+import { Avatar, Button, ConfigProvider, Layout, Typography } from 'antd';
+import { Input, Space, Alert } from 'antd';
+
+const { TextArea } = Input;
 
 export function App() {
   return (
-    <>
-      <NxWelcome title="web" />
-
-      <div />
-
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
-            </div>
-          }
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#274916',
+        },
+      }}
+    >
+      <Layout
+        style={{
+          backgroundColor: '#141414',
+          width: '100vw',
+          height: '100vh',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Typography
+          style={{
+            fontSize: '34px',
+            fontWeight: 'bold',
+            color: '#274916',
+            padding: '10px',
+            margin: '20px',
+          }}
+        >
+          Gamora AI
+        </Typography>
+        <Avatar
+          icon={<img src={gamoraAiDark} alt="" />}
+          style={{ width: 250, height: 250 }}
         />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
-          }
+
+        <TextArea
+          rows={4}
+          placeholder="Ask me something..."
+          maxLength={6}
+          size="middle"
+          style={{
+            width: '80%',
+            backgroundColor: '#7f7f7f',
+            padding: '10px',
+            margin: '20px',
+          }}
         />
-      </Routes>
-      {/* END: routes */}
-    </>
+
+        <Button type="primary" size="middle">
+          Button
+        </Button>
+
+        <Space size="large" />
+
+        <Alert
+          style={{
+            backgroundColor: '#d5f2bb',
+            width: '80%',
+            padding: '10px',
+            margin: '20px',
+          }}
+          description="Info Description Info Description Info Description Info DescriptionInfo Description Info Description Info Description Info DescriptionInfo Description Info Description Info Description Info DescriptionInfo Description Info Description Info Description Info DescriptionInfo Description Info Description Info Description Info DescriptionInfo Description Info Description Info Description Info DescriptionInfo Description Info Description Info Description Info DescriptionInfo Description Info Description Info Description Info DescriptionInfo Description Info Description Info Description Info DescriptionInfo Description Info Description Info Description Info DescriptionInfo Description Info Description Info Description Info DescriptionInfo Description Info Description Info Description Info DescriptionInfo Description Info Description Info Description Info DescriptionInfo Description Info Description Info Description Info DescriptionInfo Description Info Description Info Description Info DescriptionInfo Description Info Description Info Description Info DescriptionInfo Description Info Description Info Description Info Description"
+          type="success"
+        />
+      </Layout>
+    </ConfigProvider>
   );
 }
 
